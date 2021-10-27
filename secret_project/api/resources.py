ba6bc567af010e60ccb904944a67320aab713d1b
Andrei Luchici <andrei.luchici@daciandata.tech>
2021-10-27 21:51:02 +0300

    sample layer

--- secret_project/api/resources.py
+++ secret_project/api/resources.py
@@ -1,22 +1,28 @@
 from flask import Response
 from flask_restful import Resource, request
 
-from secret_project.api.startup import product, all_products
+from secret_project.models.product import Product, AllProducts
+from secret_project.api.startup import product_repository
+
+Product.repository = product_repository
+AllProducts.repository = product_repository
 
 
 class ProductResource(Resource):
     def get(self, id=None):
         if id is None:
-            product.prod_id = id
+            product = Product(prod_id=id)
             product.get()
             return Response(product.to_json(), status=200, content_type="application/json")
 
+        all_products = AllProducts()
         all_products.get_all()
         return Response(all_products.to_json(), status=200, content_type="application/json")
 
     def post(self):
         body = request.json
         try:
+            product = Product()
             product.create(prod_id=body["prod_id"], name=body["name"],
                            description=body["description"], price=body["price"])
             return Response(status=200, content_type="application/json")
